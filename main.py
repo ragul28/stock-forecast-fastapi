35d1b6173541da752228a04ab853e148b13baae3
ragul28 <ragulh28@gmail.com>
2024-07-28 22:17:17 +0530

    Add char validation for stock query string

--- main.py
+++ main.py
@@ -18,11 +18,17 @@ def get_status():
 
 @app.get("/forecast", response_model=StockOut, status_code=200)
 def get_prediction(stock: str = Query(..., description="Stock ticker symbol", min_length=1, max_length=5)):
+    ticker = stock.strip().upper()
+    
+    # Check if ticker contains valid chars
+    valid_chars = set(string.ascii_uppercase + string.digits + "^.-")
+    if not all(c in valid_chars for c in ticker):
+        raise HTTPException(status_code=400, detail="Invalid ticker symbol format.")
 
-    prediction_list = predict(stock)
+    prediction_list = predict(ticker)
 
     if not prediction_list:
         raise HTTPException(status_code=400, detail="Model not found.")
 
-    response_object = {"ticker": stock, "forecast": convert(prediction_list)}
+    response_object = {"ticker": ticker, "forecast": convert(prediction_list)}
     return response_object
